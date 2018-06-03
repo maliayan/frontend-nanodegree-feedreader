147f1eaa8f318ae27776b41fe9442ce46416580a
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-06-03 04:14:05 +0300

    Return the allFeeds variable to a passing state

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
  */
 
 // The names and URLs to all of the feeds we'd like available.
-var allFeedss = [
+var allFeeds = [
     {
         name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
